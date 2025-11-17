e87bdaf85a551e34251d2c9a63dad3c3f2263739
Sandra Akram <sakram@wpi.edu>
2025-11-16 22:29:13 -0500

    added is_verified column to faculty model

--- app/faculty/faculty_models.py
+++ app/faculty/faculty_models.py
@@ -9,9 +9,10 @@ from app.auth.auth_models import User
 
 class Faculty(User):
     __tablename__='faculty'
-    
-    id : sqlo.Mapped[int] = sqlo.mapped_column(sqla.ForeignKey(User.id), primary_key=True)
 
+    id : sqlo.Mapped[int] = sqlo.mapped_column(sqla.ForeignKey(User.id), primary_key=True)
+    is_verified: sqlo.Mapped[bool] = sqlo.mapped_column(default=False)
+    
     __mapper_args__ = {
     'polymorphic_identity': 'Faculty'
     }
