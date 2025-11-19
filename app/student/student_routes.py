a19092f457bebbf01e70e08dc781ac3f367ac73d
grffn9 <griffin.munhall@gmail.com>
2025-11-18 22:14:07 -0500

    Implemented student registration form, models, and html.

--- app/student/student_routes.py
+++ app/student/student_routes.py
@@ -1,16 +1,14 @@
 import sys
 from flask import render_template, flash, redirect, url_for
+from flask_login import login_required
 import sqlalchemy as sqla
 
 from app import db
-from app.student.student_models import Post
-from app.student.student_forms import PostForm
 
 from app.student import student_blueprint as bp_student
 
 @bp_student.route('/', methods=['GET'])
 @bp_student.route('/index', methods=['GET'])
+@login_required
 def index():
-    posts = db.session.scalars(sqla.select(Post).order_by(Post.timestamp.desc()))
-    all_posts  = posts.all()     
-    return render_template('index.html', title="Smile Portal", posts=all_posts)
+    return render_template('index.html')
