1bfb8fe504a156cc402930f3a81597f9999d82f7
devore002 <dorareo@wpi.edu>
2025-12-11 11:14:09 -0500

    Added faculty route protectors

--- app/faculty/faculty_routes.py
+++ app/faculty/faculty_routes.py
@@ -17,6 +17,11 @@ from app.models import Student, ResearchPosition, Application
 @bp_faculty.route('/create_position', methods=['GET', 'POST'])
 @login_required
 def create_position():
+
+    # Must be a faculty member
+    if not current_user.user_type == 'Faculty':
+        abort(403)
+
     form = ResearchPositionForm()
         
     form.preferred_majors.choices = [
@@ -194,6 +199,12 @@ def edit_position(position_id):
 @bp_faculty.route('/faculty/index', methods=['GET'])
 @login_required
 def index():
+
+    # Must be a faculty member
+    if not current_user.user_type == 'Faculty':
+        abort(403)
+
+    
     all_positions = db.session.scalars(sqla.select(ResearchPosition).where(ResearchPosition.faculty_id == current_user.id)).all()
     # all_posts  = positions.all() 
     return render_template('faculty_index.html', title="Research Portal", positions=all_positions, faculty = current_user)
@@ -201,6 +212,11 @@ def index():
 @bp_faculty.route('/faculty/profile', methods=['GET', 'POST'])
 @login_required
 def viewProfile():
+    
+    # Must be a faculty member
+    if not current_user.user_type == 'Faculty':
+        abort(403)
+
     # empty_form = EmptyForm()
     if current_user.user_type == 'Faculty':
         current_user.last_notif_time = datetime.now(timezone.utc)
@@ -212,9 +228,9 @@ def viewProfile():
 @bp_faculty.route('/faculty/<int:rec_id>/recommend', methods=['GET', 'POST'])
 @login_required
 def change_rec_status(rec_id):
-    # if current_user.user_type == 'Faculty':
-    #     current_user.last_notif_time = datetime.now(timezone.utc)
-    #     db.session.commit()
+    # Must be a faculty member
+    if not current_user.user_type == 'Faculty':
+        abort(403)
     refs = db.session.scalars(sqla.select(Application).where(Application.reference_id == current_user.id)).all()
     form = RecommendationForm()
     if form.validate_on_submit():
@@ -228,6 +244,10 @@ def change_rec_status(rec_id):
 @bp_faculty.route('/position/<int:position_id>/applicants', methods=['GET', 'POST'])
 @login_required
 def view_applicants(position_id):
+    
+    # Must be a faculty member
+    if not current_user.user_type == 'Faculty':
+        abort(403)
 
     all_applications = db.session.scalars(sqla.select(Application).where(Application.position_id == position_id)).all()
     return render_template('view_applicants.html', applications=all_applications, faculty = current_user)
@@ -236,6 +256,10 @@ def view_applicants(position_id):
 @bp_faculty.route('/position/<int:applicant_id>/', methods=['GET', 'POST'])
 @login_required
 def view_one_applicant(applicant_id):
+    
+    # Must be a faculty member
+    if not current_user.user_type == 'Faculty':
+        abort(403)
 
     student = db.session.scalars(sqla.select(Student).where(Student.id == applicant_id)).first()
     application = db.session.scalars(sqla.select(Application).where(Application.student_id == applicant_id)).first()
@@ -246,6 +270,12 @@ def view_one_applicant(applicant_id):
 @bp_faculty.route('/application/<int:app_id>/update', methods=['POST'])
 @login_required
 def update_application_status(app_id):
+
+    
+    # Must be a faculty member
+    if not current_user.user_type == 'Faculty':
+        abort(403)
+
     app = Application.query.get_or_404(app_id)
     action = request.form.get("action")
 
@@ -278,8 +308,12 @@ def update_application_status(app_id):
 @bp_faculty.route('/faculty/majors')
 @login_required
 def list_majors():
-    if current_user.user_type != "Faculty":
+    
+    # Must be a faculty member
+    if not current_user.user_type == 'Faculty':
         abort(403)
+
+
     majors = db.session.scalars(sqla.select(Major).order_by(Major.name)).all()
     return render_template('majors_list.html', majors=majors, faculty = current_user)
 
@@ -287,8 +321,10 @@ def list_majors():
 @bp_faculty.route('/faculty/majors/create', methods=['GET','POST'])
 @login_required
 def create_major():
-    if current_user.user_type != "Faculty":
+    # Must be a faculty member
+    if not current_user.user_type == 'Faculty':
         abort(403)
+
     form = MajorForm()
     if form.validate_on_submit():
         major = Major(
@@ -305,7 +341,7 @@ def create_major():
 @bp_faculty.route('/faculty/majors/<int:major_id>/edit', methods=['GET','POST'])
 @login_required
 def edit_major(major_id):
-    if current_user.user_type != "Faculty":
+    if not current_user.user_type == 'Faculty':
         abort(403)
     major = db.session.get(Major, major_id) or abort(404)
     form = MajorForm(obj=major)
@@ -321,7 +357,7 @@ def edit_major(major_id):
 @bp_faculty.route('/faculty/majors/<int:major_id>/delete', methods=['POST'])
 @login_required
 def delete_major(major_id):
-    if current_user.user_type != "Faculty":
+    if not current_user.user_type == 'Faculty':
         abort(403)
     major = db.session.get(Major, major_id) or abort(404)
     db.session.delete(major)
@@ -335,7 +371,7 @@ def delete_major(major_id):
 @bp_faculty.route('/faculty/topics')
 @login_required
 def list_topics():
-    if current_user.user_type != "Faculty":
+    if not current_user.user_type == 'Faculty':
         abort(403)
     topics = db.session.scalars(
         sqla.select(ResearchTopic).order_by(ResearchTopic.name)
@@ -362,7 +398,7 @@ def create_topic():
 @bp_faculty.route('/faculty/topics/<int:topic_id>/edit', methods=['GET','POST'])
 @login_required
 def edit_topic(topic_id):
-    if current_user.user_type != "Faculty":
+    if not current_user.user_type == 'Faculty':
         abort(403)
     topic = db.session.get(ResearchTopic, topic_id) or abort(404)
     form = ResearchTopicForm(obj=topic)
@@ -377,7 +413,7 @@ def edit_topic(topic_id):
 @bp_faculty.route('/faculty/topics/<int:topic_id>/delete', methods=['POST'])
 @login_required
 def delete_topic(topic_id):
-    if current_user.user_type != "Faculty":
+    if not current_user.user_type == 'Faculty':
         abort(403)
     topic = db.session.get(ResearchTopic, topic_id) or abort(404)
     db.session.delete(topic)
@@ -389,7 +425,7 @@ def delete_topic(topic_id):
 @bp_faculty.route('/faculty/courses')
 @login_required
 def list_courses():
-    if current_user.user_type != "Faculty":
+    if not current_user.user_type == 'Faculty':
         abort(403)
     Courses = db.session.scalars(sqla.select(Course).order_by(Course.coursenum)).all()
     return render_template('courses_list.html', courses=Courses,  faculty = current_user)
@@ -398,7 +434,7 @@ def list_courses():
 @bp_faculty.route('/faculty/courses/create', methods=['GET','POST'])
 @login_required
 def create_course():
-    if current_user.user_type != "Faculty":
+    if not current_user.user_type == 'Faculty':
         abort(403)
     form = CourseForm()
     if form.validate_on_submit():
@@ -412,7 +448,7 @@ def create_course():
 @bp_faculty.route('/faculty/courses/<int:course_id>/edit', methods=['GET','POST'])
 @login_required
 def edit_course(course_id):
-    if current_user.user_type != "Faculty":
+    if not current_user.user_type == 'Faculty':
         abort(403)
     course = db.session.get(Course, course_id) or abort(404)
     form = CourseForm(obj=course)
@@ -428,7 +464,7 @@ def edit_course(course_id):
 @bp_faculty.route('/faculty/courses/<int:course_id>/delete', methods=['POST'])
 @login_required
 def delete_course(course_id):
-    if current_user.user_type != "Faculty":
+    if not current_user.user_type == 'Faculty':
         abort(403)
     course = db.session.get(Course, course_id) or abort(404)
     db.session.delete(course)
@@ -440,7 +476,7 @@ def delete_course(course_id):
 @bp_faculty.route('/faculty/languages')
 @login_required
 def list_languages():
-    if current_user.user_type != "Faculty":
+    if not current_user.user_type == 'Faculty':
         abort(403)
     languages = db.session.scalars(
         sqla.select(ProgrammingLanguage).order_by(ProgrammingLanguage.name)
@@ -451,7 +487,7 @@ def list_languages():
 @bp_faculty.route('/faculty/languages/create', methods=['GET','POST'])
 @login_required
 def create_language():
-    if current_user.user_type != "Faculty":
+    if not current_user.user_type == 'Faculty':
         abort(403)
     form = ProgrammingLanguageForm()
     if form.validate_on_submit():
@@ -468,7 +504,7 @@ def create_language():
 @bp_faculty.route('/faculty/languages/<int:language_id>/edit', methods=['GET','POST'])
 @login_required
 def edit_language(language_id):
-    if current_user.user_type != "Faculty":
+    if not current_user.user_type == 'Faculty':
         abort(403)
     language = db.session.get(ProgrammingLanguage, language_id) or abort(404)
     form = ProgrammingLanguageForm(obj=language)
@@ -483,7 +519,7 @@ def edit_language(language_id):
 @bp_faculty.route('/faculty/languages/<int:language_id>/delete', methods=['POST'])
 @login_required
 def delete_language(language_id):
-    if current_user.user_type != "Faculty":
+    if not current_user.user_type == 'Faculty':
         abort(403)
     language = db.session.get(ProgrammingLanguage, language_id) or abort(404)
     db.session.delete(language)
@@ -494,7 +530,7 @@ def delete_language(language_id):
 @bp_faculty.route('/faculty/admin')
 @login_required
 def admin_home():
-    if current_user.user_type != "Faculty":
+    if not current_user.user_type == 'Faculty':
         abort(403)
     return render_template('faculty_admin.html', faculty = current_user)
 
